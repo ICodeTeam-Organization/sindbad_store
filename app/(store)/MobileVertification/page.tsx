ec27ddacfbaacf2ebefbc2d4ba4ecd10d4ad458e
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-07 23:37:50 +0300

    Design forgetPassword and MobileVertification Pages

---
+++ app/(store)/MobileVertification/page.tsx
@@ -0,0 +1,25 @@
+import { AiOutlineArrowRight } from "react-icons/ai"; 
+import React from 'react'
+import Link from "next/link";
+
+const MobileVert = () => {
+  return (
+    
+    <div className='m-auto mt-28 min-w-[100px] max-w-[424px] min-h-[50px] p-[32px] rounded-[4px] border-solid border-[1px] border-[#E4E7E9] shadow-lg drop-shadow-sm shadow-[#0000001F]'>
+        <h1 className='m-auto text-[28px] text-center font-bold mt-2'>التحقق من بريدك الإلكتروني </h1>
+        <div className='m-auto mt-[30px] flex justify-between'>
+            <Link className='text-[20px]  text-[#2DA5F3]' href={""}>إعادة إرسال الرمز</Link>
+            <p className='text-[20px]  text-[#191C1F]'>رمز التحقق</p>
+        </div> 
+        <div>
+            <input className='min-w-[150px] w-full h-[44px]  border-[1px] border-solid rounded-[2px] border-[#E4E7E9]' type="text" />
+            <button className='min-w-[150px] w-full h-[48px] mt-5 text-white bg-[#FA8232] hover:bg-orange-600 transition-all duration-300 rounded-[2px] text-[20px] flex justify-center items-center'>تحقق لي <AiOutlineArrowRight className="ml-3" /></button>
+        </div>   
+            
+    </div>
+    
+    
+  )
+}
+
+export default MobileVert
---
+++ app/(store)/forget/page.tsx
@@ -0,0 +1,30 @@
+import { AiOutlineArrowLeft } from "react-icons/ai"; 
+import React from 'react'
+import Link from "next/link";
+
+const forgetPass = () => {
+  return (
+    
+    <div className='m-auto mt-28 min-w-[100px] max-w-[424px] min-h-[50px] p-[32px] rounded-[4px] border-solid border-[1px] border-[#E4E7E9] shadow-lg drop-shadow-sm shadow-[#0000001F]'>
+        <div className='m-auto'>
+            <h1 className='text-[28px] text-center font-bold mt-2'>نسيت كلمة المرور</h1>
+            <p className='text-[20px] text-center text-[#5F6C72] mt-2'>أدخل رقم الهاتف المحمول المرتبط بحسابك</p>
+        </div>
+        <div className='m-auto mt-[30px]'>
+            <p className='text-[20px] text-end text-[#191C1F]'>رقم الجوال</p>
+            <input className='min-w-[150px] w-full h-[44px]  border-[1px] border-solid rounded-[2px] border-[#E4E7E9]' type="text" />
+            <button className='min-w-[150px] w-full h-[48px] mt-5 text-white bg-[#FA8232] hover:bg-orange-600 transition-all duration-300 rounded-[2px] text-[20px] flex justify-center items-center'> <AiOutlineArrowLeft className="mr-2" />إرسال الرمز</button>
+        </div>
+        <div className="text-end my-5 text-[15px] text-[#5F6C72]">
+            <p>لديك حساب بالفعل؟ <Link className="mr-1 text-[#2DA5F3]" href={""}>تسجيل الدخول</Link></p>
+            <p> ليس لديك حساب؟<Link className="mr-2 text-[#2DA5F3]" href={""}>حساب جديد </Link></p>
+        </div>
+        <hr />
+        <p className="text-end text-[#475156] mt-6">يمكنك الإتصال<Link className="text-[#F58634]" href={""}> بخدمة العملاء</Link> للحصول على المساعدة في استعادة الوصول إلى حسابك</p>
+    </div>
+    
+    
+  )
+}
+
+export default forgetPass
