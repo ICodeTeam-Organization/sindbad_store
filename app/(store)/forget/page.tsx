7eccae9d5e91b15bc1b06216139702dba71fd7de
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-08 00:08:28 +0300

    fix arabic Direction
    fix the forgetPass and mobileVert pages to match arabic direction

--- app/(store)/forget/page.tsx
+++ app/(store)/forget/page.tsx
@@ -11,16 +11,16 @@ const forgetPass = () => {
             <p className='text-[20px] text-center text-[#5F6C72] mt-2'>أدخل رقم الهاتف المحمول المرتبط بحسابك</p>
         </div>
         <div className='m-auto mt-[30px]'>
-            <p className='text-[20px] text-end text-[#191C1F]'>رقم الجوال</p>
+            <p className='text-[20px] text-start text-[#191C1F]'>رقم الجوال</p>
             <input className='min-w-[150px] w-full h-[44px]  border-[1px] border-solid rounded-[2px] border-[#E4E7E9]' type="text" />
-            <button className='min-w-[150px] w-full h-[48px] mt-5 text-white bg-[#FA8232] hover:bg-orange-600 transition-all duration-300 rounded-[2px] text-[20px] flex justify-center items-center'> <AiOutlineArrowLeft className="mr-2" />إرسال الرمز</button>
+            <button className='min-w-[150px] w-full h-[48px] mt-5 text-white bg-[#FA8232] hover:bg-orange-600 transition-all duration-300 rounded-[2px] text-[20px] flex justify-center items-center'>إرسال الرمز <AiOutlineArrowLeft className="mr-2" /></button>
         </div>
-        <div className="text-end my-5 text-[15px] text-[#5F6C72]">
+        <div className="text-start my-5 text-[15px] text-[#5F6C72]">
             <p>لديك حساب بالفعل؟ <Link className="mr-1 text-[#2DA5F3]" href={""}>تسجيل الدخول</Link></p>
             <p> ليس لديك حساب؟<Link className="mr-2 text-[#2DA5F3]" href={""}>حساب جديد </Link></p>
         </div>
         <hr />
-        <p className="text-end text-[#475156] mt-6">يمكنك الإتصال<Link className="text-[#F58634]" href={""}> بخدمة العملاء</Link> للحصول على المساعدة في استعادة الوصول إلى حسابك</p>
+        <p className="text-start text-[#475156] mt-6">يمكنك الإتصال<Link className="text-[#F58634]" href={""}> بخدمة العملاء</Link> للحصول على المساعدة في استعادة الوصول إلى حسابك</p>
     </div>
     
     
