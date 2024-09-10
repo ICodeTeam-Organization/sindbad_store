9a3f40c1f6091fb5e5b5a0379c8c6fde29de5503
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-10 09:50:14 +0300

    style: put responsive of shiping adress page

---
+++ app/(store)/ShipingAdress/Components/Pagination.tsx
@@ -0,0 +1,16 @@
+import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs'
+
+const Pagination = () => {
+    const arr = ['01','02','03','04','05'];
+    return (
+    <div className="flex items-center justify-center cursor-pointer mt-16 mb-10">
+        <BsArrowRightCircle className="text-primary-background mx-3 hover:text-white hover:bg-primary-background transition-all duration-700 rounded-full" size={30}/>
+        {arr.map((itm) => (
+            <div className=" rounded-full border-2 hover:text-white hover:bg-primary-background hover:border-none transition-all duration-700 w-8 h-8 p-1 text-center ml-1">{itm}</div>
+        ))}
+        <BsArrowLeftCircle className="text-primary-background mx-3 hover:text-white hover:bg-primary-background transition-all duration-700 rounded-full" size={30}/>
+    </div>
+  )
+}
+
+export default Pagination
--- app/(store)/ShipingAdress/page.tsx
+++ app/(store)/ShipingAdress/page.tsx
@@ -3,21 +3,15 @@ import { BsArrowLeftCircle } from "react-icons/bs";
 import { BiPlusCircle } from "react-icons/bi"; 
 import Adresses from './Components/Adresses'
 import AddShipingAdresses from'./Components/AddShipingAdresses'
+import Pagination from "./Components/Pagination";
 const ShipingAdr = () => {
-  const arr = ['01','02','03','04','05'];
   return (
     <div className="m-auto my-16">
         <AddShipingAdresses />
         <div className="mx-8 m-auto border-2 rounded-sm py-3 mt-6">
             <p className="pr-6 pb-3 text-lg font-bold">عناوين المستخدم</p>
             <Adresses />
-            <div className="flex items-center justify-center cursor-pointer mt-16 mb-10">
-                <BsArrowRightCircle className="text-primary-background mx-3 hover:text-white hover:bg-primary-background transition-all duration-700 rounded-full" size={30}/>
-                {arr.map((itm) => (
-                  <div className=" rounded-full border-2 hover:text-white hover:bg-primary-background hover:border-none transition-all duration-700 w-8 h-8 p-1 text-center ml-1">{itm}</div>
-                ))}
-                <BsArrowLeftCircle className="text-primary-background mx-3 hover:text-white hover:bg-primary-background transition-all duration-700 rounded-full" size={30}/>
-            </div>
+            <Pagination/>
         </div>
     </div>
   )
