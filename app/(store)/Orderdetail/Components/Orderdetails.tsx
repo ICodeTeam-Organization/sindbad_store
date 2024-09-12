5936f663a32d0d2ab72f4b17ce50d6a0f9e39ad0
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-12 10:58:32 +0300

    style: remove static speech and add array

--- app/(store)/Orderdetail/Components/Orderdetails.tsx
+++ app/(store)/Orderdetail/Components/Orderdetails.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import Stores from "@/public/images/Stores.svg";
 const Adresses = () => {
-  const arr = [1, 2, 3, 4, 5];
+  const data = [
+    {
+       title : "4K UHD LED Smart TV with Chromecast Built-in",
+       image: Stores,
+       price: 70,
+       quantity: 1
+    },
+     {
+       title : "4K UHD LED Smart TV with Chromecast Built-in",
+       image: Stores,
+       price: 140,
+       quantity: 6
+    },
+ ];
   return (
     <div className="grid grid-cols-6 text-center items-center font-bold w-full text-gray-600">
       <h1 className="bg-gray-200 py-1 border-t-2 border-b-2 w-full col-span-3 text-xl max-sm:text-sm text-right pr-5">
@@ -18,17 +31,17 @@ const Adresses = () => {
       </h1>
 
       {/* fetch api from here */}
-      {arr.map((itm) => (
+      {data.map((itm) => (
         <>
           <div className="flex justify-between items-center text-right px-3 max-md:text-xs  mt-9 col-span-3">
-            <Image src={Stores} alt={""} className="" width={50} />
-            <h1 className="pr-3 line-clamp-3">
-              4K UHD LED Smart TV with Chromecast Built-in
+            <Image src={itm.image} alt={itm.title} className="" width={50} />
+            <h1 className=" pr-3 line-clamp-3">
+              {itm.title}
             </h1>
           </div>
-          <h1 className="m-auto px-3 line-clamp-3 max-sm:text-xs mt-9">$70</h1>
-          <h1 className="m-auto mt-9 line-clamp-3 max-sm:text-xs">01</h1>
-          <h1 className="m-auto mt-9 line-clamp-3 max-sm:text-xs">$70</h1>
+          <h1 className="m-auto px-3 line-clamp-3 max-sm:text-xs mt-9">{itm.price}$</h1>
+          <h1 className="m-auto mt-9 line-clamp-3 max-sm:text-xs">{itm.quantity}</h1>
+          <h1 className="m-auto mt-9 line-clamp-3 max-sm:text-xs">{itm.price*itm.quantity}</h1>
         </>
       ))}
     </div>
--- app/(store)/Orders/Components/MyOrders.tsx
+++ app/(store)/Orders/Components/MyOrders.tsx
@@ -1,5 +1,20 @@
 const Adresses = () => {
-  const arr = [1, 2, 3, 4, 5];
+  const data = [
+    {
+       title : "جوال سامسونج - كفر جوال",
+       price: 2100.00,
+       date: "15/10/2024",
+       state:"في الطريق",
+       track:"تتبع الطلب"
+    },
+     {
+       title : "4K UHD LED Smart TV with Chromecast Built-in",
+       price: 3000.00,
+       date: "15/10/2024",
+       state:"تم التسليم",
+       track:"تتبع الطلب"
+    },
+ ];
   return (
     <div className="grid grid-cols-5 text-center items-center font-bold w-full text-gray-600">
       <h1 className="bg-gray-200 py-1 border-t-2 border-b-2 w-full">
@@ -11,22 +26,22 @@ const Adresses = () => {
       <h1 className="bg-gray-200 py-1 border-t-2 border-b-2">تتبع</h1>
 
       {/* fetch api from here */}
-      {arr.map((itm) => (
+      {data.map((itm) => (
         <>
           <h1 className="text-right line-clamp-3 px-3 max-md:text-[10px] max-md:line-clamp-4 mt-9">
-            جوال سامسونج - كفر جوال
+            {itm.title}
           </h1>
           <h1 className="m-auto text-gray-500 px-3 line-clamp-3 text-right max-md:text-xs max-md:line-clamp-4 mt-9">
-            2100.00
+          {itm.price}
           </h1>
           <h1 className="m-auto px-3 line-clamp-3 max-sm:text-[9px] mt-9">
-            15/10/2024
+          {itm.date}
           </h1>
           <h1 className="m-auto mt-9 line-clamp-3 text-blue-400 max-sm:text-xs">
-            في الطريق
+          {itm.state}
           </h1>
           <h1 className="m-auto mt-9 line-clamp-3 text-blue-400 max-sm:text-xs">
-            تتبع الطلب
+          {itm.track}
           </h1>
         </>
       ))}
