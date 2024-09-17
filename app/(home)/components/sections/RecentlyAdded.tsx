c1559cb2ba16090f9237fb6b2cb3a18f39a7ecf1
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-17 16:32:16 +0300

    feat: add APIs to components in home page

--- app/(home)/components/sections/RecentlyAdded.tsx
+++ app/(home)/components/sections/RecentlyAdded.tsx
@@ -1,51 +1,24 @@
 import SectionTitle from "../SectionTitle";
-import hero from "@/public/images/hero.jpg";
 import ProductCard from "../ProductCard";
+import { getApi } from "@/lib/http";
+import { notFound } from "next/navigation";
+const RecentlyAdded = async () => {
+  const RecentlyProducts = await getApi<any>(
+    "Product/Market/HomePage/GetLastProductsAddedToMarketForViewInMarketHomePage/5"
+  );
+  if (!RecentlyProducts) return notFound;
 
-const RecentlyAdded = () => {
-  const products = [
-    {
-      Image: hero.src,
-      productName: "ابريق شاي زجاجي مع امكانية التفاف النص في اسم الصنف",
-      price: 1200,
-      oldPrice: 1600,
-    },
-    {
-      Image: hero.src,
-      productName: "ابريق شاي زجاجي مع امكانية التفاف النص في اسم الصنف",
-      price: 1200,
-      oldPrice: 1600,
-    },
-    {
-      Image: hero.src,
-      productName: "ابريق شاي زجاجي مع امكانية التفاف النص في اسم الصنف",
-      price: 1200,
-      oldPrice: 1600,
-    },
-    {
-      Image: hero.src,
-      productName: "ابريق شاي زجاجي مع امكانية التفاف النص في اسم الصنف",
-      price: 1200,
-      oldPrice: 1600,
-    },
-    {
-      Image: hero.src,
-      productName: "ابريق شاي زجاجي مع امكانية التفاف النص في اسم الصنف",
-      price: 1200,
-      oldPrice: 1600,
-    },
-  ];
   return (
     <div className="container pt-10 mx-autocontainer mx-auto sm:px-4 md:px8 lg:px16 xl:px-32">
       <SectionTitle title={"اضيفت مؤخرا"} />
       <div className="container ">
         <div className="grid 2xl:grid-cols-5 lg:grid-cols-4 max-lg:grid-cols-4 max-sm:grid-cols-3 gap-y-5 2xl:gap-[25px] max-sm:gap-x-1 max-lg:gap-x-5  max-md:m-auto  py-4 mr-2 ">
-          {products.map((product) => (
+          {RecentlyProducts.data.map((product: any) => (
             <ProductCard
-              image={product.Image}
-              productName={product.productName}
+              key={product.id}
+              image={product.mainImageUrl}
+              productName={product.name}
               price={product.price}
-              oldPrice={product.oldPrice}
             />
           ))}
         </div>
---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img-cdn.pixlr.com",
+        port: "",
+        pathname: "/image-generator/history/**",
+      }
+    ],
+  },
+};
