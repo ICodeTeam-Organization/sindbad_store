0c3932d550079b89ebbf6db7bb10d7ee89ee44ac
Abdulrahman Al-attas <a.omar.al2001@gmail.com>
2024-09-22 18:56:37 +0300

    feat: add nav bar in home page

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,39 +1,66 @@
-"use client";
-import { AiOutlineClose } from "react-icons/ai";
-import Link from "next/link";
-import { useState } from "react";
 import { BiMenu } from "react-icons/bi";
-
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import { GoPerson } from "react-icons/go";
 const Navbar = () => {
-  let [Nav, setNav] = useState(false);
-
   return (
-    <div>
-      <div className="container mx-auto px-4">
-        <div>
-          <button>
-            {Nav ? (
-              <AiOutlineClose onClick={() => setNav((nav) => !nav)} size={35} />
-            ) : (
-              <BiMenu onClick={() => setNav((nav) => !nav)} size={35} />
-            )}
-          </button>
+    <Sheet>
+      <SheetTrigger asChild>
+        <BiMenu className="cursor-pointer" size={35} />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <Link href={""}>
+            <GoPerson size={100} />
+          </Link>
+          <br />
+          <SheetTitle>تعديل الملف الشخصي</SheetTitle>
+        </SheetHeader>
+        <br />
+        <br />
+        <div className="grid gap-4 py-4">
+          <Link href={"/Orders"}>
+            <Label
+              htmlFor="name"
+              className="text-right text-2xl cursor-pointer"
+            >
+              طلباتي
+            </Label>
+          </Link>
+
+          <Link className="mt-2" href={"/ShipingAdress"}>
+            <Label
+              htmlFor="username"
+              className="text-right text-2xl cursor-pointer"
+            >
+              إضافة عنوان جديد
+            </Label>
+          </Link>
+          <Link className="mt-2 absolute bottom-8" href={"#"}>
+            <Label
+              htmlFor="username"
+              className="text-right text-2xl cursor-pointer text-red-500"
+            >
+              تسجيل الخروج
+            </Label>
+          </Link>
         </div>
-        {Nav ? (
-          <nav className="w-24 absolute bg-slate-600 transition-all duration-1000">
-            <Link className=" block" href={""}>
-              الصفحة الرئيسية
-            </Link>
-            <Link className=" block" href={""}>
-              حولنا
-            </Link>
-            <Link className=" block" href={""}>
-              تواصل معنا
-            </Link>
-          </nav>
-        ) : null}
-      </div>
-    </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            {/* <Button type="submit">Save changes</Button> */}
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
 
