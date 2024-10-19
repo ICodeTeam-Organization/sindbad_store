56dd03eabf204b4e42fff5948e082ade55fa0ca6
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-10-19 18:01:46 +0300

    fix: add to cart in product detiles

--- app/(store)/shop/productDetils/[productId]/components/AddToBasket.tsx
+++ app/(store)/shop/productDetils/[productId]/components/AddToBasket.tsx
@@ -4,6 +4,9 @@ import axios from "axios";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@radix-ui/react-toast";
 import { Loader2 } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 
 type AddToBasketProps = {
   productId: string;
@@ -12,20 +15,26 @@ type AddToBasketProps = {
 
 const AddToBasket = ({ productId, quantity }: AddToBasketProps) => {
   const { toast } = useToast();
+  const { data: session, status } = useSession();
+  const redirct = useRouter();
+
 
   const mutation = useMutation({
     mutationFn: async () => {
       const res = await axios.post(
-        `https://icode-sendbad-store.runasp.net/AddProductToCart?productId=${productId}`,
-        { quantity },
+        `https://icode-sendbad-store.runasp.net/api/Cart/AddProductToCart?productId=${productId}`,
+        {
+          quantity: 1,
+        },
         {
           headers: {
             "Accept-Language": "ar",
-            "Content-Type": "application/json",
+            "Content-type": "multipart/form-data",
+            Authorization: `Bearer ${session?.user.data.token}`,
           },
         }
       );
-      return res;
+      // return res;
     },
     onSuccess: () => {
       toast({
@@ -36,21 +45,28 @@ const AddToBasket = ({ productId, quantity }: AddToBasketProps) => {
         },
       });
     },
-    onError: () => {
+    onError: (res: any) => {
       toast({
         variant: "destructive",
-        description: "حدث خطأ اثناء عملية الاضافة الى السلة",
+        description: res.response.data.message,
         action: <ToastAction altText="Try again">حاول مرة اخرى</ToastAction>,
       });
     },
   });
+
+  const handleAddToCart = () => {
+    if (status === "unauthenticated") redirct.push("/auth");
+    else if (status === "authenticated") {
+      mutation.mutate();
+    }
+  };
 
   const { isPending } = mutation;
 
   return (
     <button
       className="min-w-[200px] h-[50px] bg-orange-500 text-white text-xl rounded-md flex justify-center items-center mb-2 md:mb-0"
-      onClick={() => mutation.mutate()}
+      onClick={() => handleAddToCart()}
       disabled={isPending}
     >
       {isPending ? <Loader2 className="animate-spin" /> : <p>اضف للسلة</p>}
