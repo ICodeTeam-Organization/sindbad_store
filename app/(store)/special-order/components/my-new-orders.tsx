0334d5e9ec933d5f502727df76d9b5a518f6c433
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-10-27 10:27:20 +0300

    Edit SpecialOrder Design

--- app/(store)/special-order/components/my-new-orders.tsx
+++ app/(store)/special-order/components/my-new-orders.tsx
@@ -30,18 +30,18 @@ const MyNewOrder = () => {
   // console.log(AllNewOrders)
   return (
     <Card className="rounded-none border-black p-6">
-      <div className="flex justify-around items-center border m-auto border-black  py-2">
+      <div className="flex flex-col gap-2 sm:flex-row justify-around items-center border m-auto border-black  py-2">
         <div className="flex items-center m-auto">
           <h1 className="ml-2 m-auto">نوع الطلب</h1>
           <Dropdown name="منتج" />
         </div>
         <div className="flex items-center m-auto">
           <h1 className="ml-2 m-auto">الفئة الرئيسية :</h1>
-          <Dropdown name="" />
+          <Dropdown name="اختر الفئة" />
         </div>
         <div className="flex items-center m-auto">
           <h1 className="ml-2 m-auto">الفئة الفرعية :</h1>
-          <Dropdown />
+          <Dropdown name="اختر الفئة"/>
         </div>
       </div>
       <div className="row">
