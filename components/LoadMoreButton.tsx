3ea8e5299ec7758c774a7379cadd1406dfaa4f5d
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-10-31 01:16:34 +0300

    Create LoadMoreButton.tsx

---
+++ components/LoadMoreButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { BiArrowFromTop } from "react-icons/bi";
+
+interface LoadMoreButtonProps {
+    onClick: () => void;
+    isLoading: boolean;
+}
+
+const LoadMoreButton: React.FC<LoadMoreButtonProps> = ({ onClick, isLoading }) => {
+    return (
+        <Button
+            onClick={onClick}
+            disabled={isLoading}
+            variant="outline"
+            className="mx-auto my-2 py-2 px-6 flex justify-between items-center bg-primary-background hover:bg-orange-600 hover:text-white text-white rounded-sm"
+        >
+            عرض المزيد
+            {isLoading ? <Loader2 className="animate-spin" /> : <BiArrowFromTop className="mr-1" size={20} />}
+        </Button>
+    );
+};
+
+export default LoadMoreButton;
