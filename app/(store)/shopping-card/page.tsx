91e663a61c4780f8501f561d9bba7f546e6d1bb6
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-11-12 22:44:12 +0300

    ايقاف كود في السله

--- app/(store)/shopping-card/page.tsx
+++ app/(store)/shopping-card/page.tsx
@@ -5,12 +5,12 @@ import ShoppingCart from "./components/shopping-card";
 const page = () => {
   return (
     <div className="container mx-auto sm:px-4 md:px-8 lg:px-16 xl:px-32  py-20 ">
-      <div className="flex items-center w-full px-4">
+      {/* <div className="flex items-center w-full px-4">
         <SelectShppingAdress />
         <div className="mr-3 flex items-center">
           <span className="ml-3">مستعجلة</span> <Checkbox />
         </div>
-      </div>
+      </div> */}
       <ShoppingCart />
     </div>
   );
