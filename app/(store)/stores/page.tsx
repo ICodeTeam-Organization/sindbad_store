003c6d87fb72a008acbd5b86a456f7ccb52633c5
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-11-16 00:25:16 +0300

    الاستضافه

--- app/(store)/stores/page.tsx
+++ app/(store)/stores/page.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import StoreGrid from "./components/store-grid";
-import { getApi } from "@/lib/http";
 
 const StoresPage = async () => {
-  const stores = await getApi<any>(
-    "Market/Store/GetAllStoresThatHasLinksToOnlineWebsite"
-  );
 
   return (
   
     <div className="mt-12">
-      <StoreGrid  allStores={stores} />
+      <StoreGrid  />
     </div>
   );
 }
