96b651aea76439fe9f76edfd430b96500e4bd7fa
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-11-26 21:39:47 +0300

    Add: new domin

--- next.config.js
+++ next.config.js
@@ -43,6 +43,42 @@ module.exports = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
+
+/* 
+
+مثال باستخدام متغيرات البيئة:
+إنشاء متغير بيئي:
+في ملف .env.local، يمكنك إضافة أسماء النطاقات:
+plaintext
+
+Copy
+IMAGE_DOMAINS=https://example.com,https://ik.imagekit.io
+تعديل ملف التكوين:
+في ملف next.config.js، يمكنك تقسيم النطاقات إلى مصفوفة واستخدامها:
+javascript
+
+Copy
+const domains = process.env.IMAGE_DOMAINS.split(',');
+
+module.exports = {
+  images: {
+    remotePatterns: domains.map(domain => ({
+      protocol: domain.startsWith('https') ? 'https' : 'http',
+      hostname: domain.replace(/https?:\/\//, ''),
+      port: '',
+      pathname: '/**',
+    })),
+  },
+};
+
+
+*/
