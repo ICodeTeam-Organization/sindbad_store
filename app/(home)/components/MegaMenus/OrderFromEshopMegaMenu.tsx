abd52ba4d98a86793e4edd71558fc81b7b9bdb37
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-11-28 14:06:49 +0300

    Update OrderFromEshopMegaMenu.tsx

--- app/(home)/components/MegaMenus/OrderFromEshopMegaMenu.tsx
+++ app/(home)/components/MegaMenus/OrderFromEshopMegaMenu.tsx
@@ -32,7 +32,7 @@ function OrderFromEshopMegaMenu() {
     if (allMainCat.length > 0) {
         setParams(o=>({...o,selectedCategory:allMainCat[0]?.id}))
     }
-  },[categories])
+  },[allMainCat, categories])
   return (
     <div className="transition-all duration-200 right-0 opacity-0 invisible hidden  mdHalf:block  group-hover:block  translate-y-5  group-hover:-translate-y-0 mdHalf:w-[85%] w-full group-hover:opacity-100 group-hover:visible mdHalf:mt-1 -mt-2 rounded top-10 left-0   max-h-[400px] mdHalf:overflow-y-hidden overflow-y-scroll z-[99999]  bg-white  mdHalf:shadow-md mdHalf:border-y border-b dark:bg-gray-800  mdHalf:absolute   ">
       <div className="flex mdHalf:flex-row flex-col px-4 py-5 mx-auto text-sm text-gray-500 dark:text-gray-400 md:px-6 gap-x-4 w-full">
