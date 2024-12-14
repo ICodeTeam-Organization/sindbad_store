082cee0e08af45a15f9650cd254ede4d0b9a0f82
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-12-14 21:33:56 +0300

    Update types.ts

--- app/(store)/shop/productDetils/[productId]/types.ts
+++ app/(store)/shop/productDetils/[productId]/types.ts
@@ -60,8 +60,50 @@ export interface ReviewProps {
   reviewDate: string;
   customerImage: string | null;
   numOfRate: number;
+  isDeleted: boolean;
 };
+// interface Review {
+//   customerName: string;     
+//   reviewText: string;     
+//   reviewDate: string;       
+//   customerImage: string;    
+//   numOfRate: number;        
+//   isDeleted: boolean;        
+// }
 
 export interface ReviewFormProps {
   productId: number;
 }
+
+export interface ProductImage {
+  imageUrl: string;
+}
+export interface AttributeWithValues {
+  attributeName: string;
+  values: string[];
+}
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  priceBeforOffer: number;
+  priceAfterOffer: number;
+  percentageOfDiscount: number;
+  amountYouShouldToBuyForGetOffer: number;
+  amountYouWillGetFromOffer: number;
+  offerSentence: string;
+  offerStartDate: string; 
+  offerEndDate: string; 
+  mainImageUrl: string;
+  number: string;
+  brandName: string;
+  categoryName: string;
+  oneStarCount: number;
+  twoStarCount: number;
+  threeStarCount: number;
+  fourStarCount: number;
+  fiveStarCount: number;
+  productImages: ProductImage[];
+  attributesWithValues: AttributeWithValues[];
+}
+
