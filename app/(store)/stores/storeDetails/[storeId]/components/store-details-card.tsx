1af7fcbf807ee9b78351dd66e44ca62ac43a770e
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2024-12-23 17:35:16 +0300

    اكمال تفاصيل المتجر مع الربط

--- app/(store)/stores/storeDetails/[storeId]/components/store-details-card.tsx
+++ app/(store)/stores/storeDetails/[storeId]/components/store-details-card.tsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 import { StoreData } from "../../../typest";
 import { IoMdHeartEmpty } from "react-icons/io";
 import SafeImage from "@/components/SafeImage";
+// import { Link } from "lucide-react";
+import Link from "next/link";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const StoreDetailsCard = ({id,
   name ,
@@ -18,23 +27,58 @@ const StoreDetailsCard = ({id,
             <SafeImage
           src={imageUrl}
           alt={name}
-          className="w-full h-[400px] object-cover rounded-lg"
+          className="w-full h-[400px] object-cover rounded-lg border shadow-md"
           width={380}
           height={250}
       />
       <div className="m-4">
         <h2 className="font-bold mt-2 text-center">{name}</h2>
         <p className="text-gray-extr-light mt-2">{description}</p>
+        {storeCategoriesIds? (
+        <p className="text-gray-extr-light mt-2">{
+          storeCategoriesIds?.map((category) => (
+            <span key={category.id} className="border-2 border-gray-300 px-2 py-1 text-sm rounded-md">
+              {category.categoryName}
+            </span>
+        ))}</p>
+        ): null
+      }
+
+      {
+        images? (
+          <Carousel>
+            <CarouselContent>
+              {images.map((image) => (
+                <CarouselItem key={image.id} className="sm:basis-1/2 lg:basis-1/3">
+                  <SafeImage
+                    src={image.imageUrl}
+                    alt={name}
+                    className="w-full h-[400px] object-cover rounded-lg border shadow-md"
+                    width={380}
+                    height={250}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        ) : null
+      }
+
         <div className="flex flex-wrap justify-evenly items-center w-full mt-6 px-4">
-          <button className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
-            المتجر
-          </button>
-          <button className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
-            الصور
-          </button>
-          <button className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
+          <Link href={"/shop?storeId="+id} className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
+            منتجات المتجر
+          </Link>
+          {
+            websiteUrl != null?
+          <Link href={websiteUrl} target="_blank" className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
             متجر المحل
+          </Link> :
+            <button className="flex-1 min-w-[70px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
+            لايوجد رابط
           </button>
+          }
           <button className="flex-1 min-w-[40px] h-[30px] border border-gray text-black text-base rounded-md flex justify-center items-center mx-1">
             <IoMdHeartEmpty className="w-4 h-4 " />
           </button>
